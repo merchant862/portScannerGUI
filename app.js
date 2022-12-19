a20aa2c9a47c1358836ed1bc16554979dff3e49a
Saif Ali <43372874+merchant862@users.noreply.github.com>
2022-12-19 21:46:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var morgan = require('morgan')
 var helmet = require('helmet')
 
 var indexRouter = require('./routes/index');
@@ -14,7 +13,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('tiny'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
